aef3a044c393dc21847c8c9bfff78b3e22eb69a6
瑞基 伊藤 <150396@jc-21.jp>
2017-12-01 09:29:54 +0900

    BMIの値から体型を表示する処理

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -28,7 +28,33 @@ namespace BMI
 
             double dBMI = dWeight / dHeight / dHeight;
 
-            DisplayAlert("BMI", dBMI+"太りすぎ", "OK");
+            if (dBMI < 18.5)
+            {
+                DisplayAlert("BMI", dBMI + "：低体重", "OK");
+            }
+            else if (dBMI < 25.0)
+            {
+                DisplayAlert("BMI", dBMI + "：普通体重", "OK");
+            }
+            else if (dBMI < 30.0)
+            {
+                DisplayAlert("BMI", dBMI + "：肥満（1度）", "OK");
+            }
+            else if (dBMI < 35.0)
+            {
+                DisplayAlert("BMI", dBMI + "：肥満（2度）", "OK");
+            }
+            else if (dBMI < 40.0)
+            {
+                DisplayAlert("BMI", dBMI + "：肥満（3度）", "OK");
+            }
+            else
+            {
+                DisplayAlert("BMI", dBMI + "：肥満（4度）", "OK");
+            }
+
+
+
         }
     }
 }
